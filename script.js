481d4dacd0b7d1bba20637a3dfc1febf493fea26
squarefacebandit <squarefacebandit@outlook.com>
2017-07-21 16:09:00 +0200

    edited

--- script.js
+++ script.js
@@ -128,10 +128,3 @@ for (var i = 1; i <= 100; i++)
     }
 }
 
-The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% of programming job candidates who can't seem to program their way out of a wet paper bag. The text of the programming assignment is as follows:
-
-"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
-
-Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp
-
-*/
